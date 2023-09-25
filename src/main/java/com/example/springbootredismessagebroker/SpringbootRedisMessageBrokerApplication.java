5065c147e3cab5172ab9a94256fd5b195aade8f9
sagar.shrestha <sagar.shrestha@f1soft.com>
2023-09-25 09:10:32 +0545

    Initiate

---
+++ src/main/java/com/example/springbootredismessagebroker/SpringbootRedisMessageBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootredismessagebroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisMessageBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisMessageBrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootredismessagebroker/SpringbootRedisMessageBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootredismessagebroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisMessageBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
